a6d9947fe580e6490943f3c53c45b5b3809b9a0f
cem <cemyilmaztech@gmail.com>
2024-08-29 19:09:32 +0300

    page created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import React, { useState } from 'react';
+import List from './components/List';
+import "./index.scss"
+import data from "./helper/data.js"
+
 function App() {
+  const [kişi, setKişi] = useState("")
+  const handleClick=()=>{
+    setKişi(kişi)
+  }
+ 
   return (
-    <div className="App">
-      <h1>Cohort 17</h1>
-    </div>
+    <main>
+      <section className="container">
+        <h3>Employee List</h3>
+        <h5>
+          (Employees 1 to 5)
+        </h5><div>
+          {data.map((employee)=>(
+  <List {...employee}/>
+          ))}
+        </div>
+      
+        <div className='btns'>
+          <button >Previous</button>
+          <button >Next</button>
+        </div>
+      </section>
+    </main>
   );
 }
 
